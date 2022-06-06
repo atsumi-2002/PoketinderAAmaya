b17abce98bfb9e794a4119961376a01183210a83
linder3hs <linderhassinger00@gmail.com>
2022-06-05 19:54:33 -0500

    Initial Commit

---
+++ iOSAppTemplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSAppTemplate
+//
+//  Created by Linder Anderson Hassinger Solano    on 5/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
